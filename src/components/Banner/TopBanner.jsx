075741fef1aab1507165a9a8c4520cb74bc13097
Sahil Abrar <sahilabrar444@gmail.com>
2024-02-21 14:11:08 +0600

    added image position

--- src/components/Banner/TopBanner.jsx
+++ src/components/Banner/TopBanner.jsx
@@ -1,21 +1,37 @@
-
 import Image from 'next/image';
-import img1 from '../../../public/landing-R.png'
-import img2 from '../../../public/landing-O.png'
-import img3 from '../../../public/landing-A.png'
-import img4 from '../../../public/landing-M.png'
+import img1 from '../../../public/landing-R.png';
+import img2 from '../../../public/landing-O.png';
+import img3 from '../../../public/landing-A.png';
+import img4 from '../../../public/landing-M.png';
+import img5 from '../../../public/landing-slider-img-1.png';
 
 const TopBanner = () => {
   return (
     <div>
-     <div>
-       <div>
-      <Image src={img1} alt=''></Image>
+      <div className="relative pt-20 pb-96">
+        <div className="flex justify-center">
+          <div>
+            <Image src={img1} alt=""></Image>
+          </div>
+          <div>
+            <Image src={img2} alt=""></Image>
+          </div>
+        </div>
+        <div className="flex justify-center pr-8">
+          <div>
+            <Image src={img3} alt=""></Image>
+          </div>
+          <div>
+            <Image src={img4} alt=""></Image>
+          </div>
+        </div>
+
+        <div className="absolute top-24 left-64">
+          <div>
+            <Image src={img5} alt=""></Image>
+          </div>
+        </div>
       </div>
-      <div>
-      <Image src={img2} alt=''></Image>
-      </div>
-     </div>
     </div>
   );
 };
