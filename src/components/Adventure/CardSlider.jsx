5e5e5100ba9501a467a648447d01bac196e2cf82
Sahil Abrar <sahilabrar444@gmail.com>
2024-02-22 21:03:37 +0600

    fetching card data

--- src/components/Adventure/CardSlider.jsx
+++ src/components/Adventure/CardSlider.jsx
@@ -10,7 +10,7 @@ const CardSlider = ({img}) => {
         className=" bg-cover group bg-center h-[400px]  rounded-lg"
         style={{ background: `url('${img}')` }}
       >
-        <div className="bg-white transition-all text-black mx-5 h-[136px] group-hover:h-[253px] w-[255px]  rounded-xl p-6 absolute bottom-14 ">
+        {/* <div className="bg-white transition-all text-black mx-5 h-[136px] group-hover:h-[253px] w-[255px]  rounded-xl p-6 absolute bottom-14 ">
           <div className="flex items-center justify-between">
             <h1 className="text-2xl relative adventure-title flex flex-col-reverse font-bold mb-2">
               Couple <br /> camping or <br /> cabin
@@ -22,7 +22,7 @@ const CardSlider = ({img}) => {
           <p className="invisible opacity-0 group-hover:visible group-hover:opacity-100 delay-75">
             Lorem ipsum <br /> dolor sit amet <br /> consectetur.
           </p>
-        </div>
+        </div> */}
       </div>
     );
 };
