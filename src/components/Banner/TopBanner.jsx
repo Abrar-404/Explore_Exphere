19948b2eebc211fa09f984fec72f0737ff37b567
Sahil Abrar <sahilabrar444@gmail.com>
2024-02-22 21:22:37 +0600

    added something

--- src/components/Banner/TopBanner.jsx
+++ src/components/Banner/TopBanner.jsx
@@ -29,7 +29,7 @@ const TopBanner = () => {
 
         <div className="absolute lg:top-24 lg:left-64 md:top-24 md:left-0">
           <div className="">
-            <Image className="scale-in-center" src={img5} alt=""></Image>
+            {/* <Image className="scale-in-center" src={img5} alt=""></Image> */}
           </div>
         </div>
       </div>
