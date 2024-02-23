9bf4e3d2dfb66b6caee479a23337be48dbcdf1dd
Sahil Abrar <sahilabrar444@gmail.com>
2024-02-23 18:32:19 +0600

    adding spacing in banner

--- src/components/Banner/TopBanner.jsx
+++ src/components/Banner/TopBanner.jsx
@@ -9,27 +9,29 @@ import img5 from '../../../public/landing-slider-img-1.png';
 const TopBanner = () => {
   return (
     <div>
-      <div className="relative pt-20 pb-96">
-        <div className="flex justify-center">
-          <div>
-            <Image className=" roll-in-left" src={img1} alt=""></Image>
+      <div className="border-2 border-red-600 max-w-[600px] mx-auto justify-center flex">
+        <div className="relative pt-20 pb-96">
+          <div className="flex justify-center">
+            <div>
+              <Image className=" roll-in-left" src={img1} alt=""></Image>
+            </div>
+            <div>
+              <Image className=" roll-in-right" src={img2} alt=""></Image>
+            </div>
           </div>
-          <div>
-            <Image className=" roll-in-right" src={img2} alt=""></Image>
+          <div className="flex justify-center pr-8">
+            <div>
+              <Image className=" roll-in-left" src={img3} alt=""></Image>
+            </div>
+            <div>
+              <Image className=" roll-in-right" src={img4} alt=""></Image>
+            </div>
           </div>
-        </div>
-        <div className="flex justify-center pr-8">
-          <div>
-            <Image className=" roll-in-left" src={img3} alt=""></Image>
-          </div>
-          <div>
-            <Image className=" roll-in-right" src={img4} alt=""></Image>
-          </div>
-        </div>
 
-        <div className="absolute lg:top-24 lg:left-64 md:top-24 md:left-0">
-          <div className="">
-            <Image className="scale-in-center" src={img5} alt=""></Image>
+          <div className="absolute lg:top-24 lg:left-64 md:top-24 md:left-0">
+            <div className="w-[500px] h-[400px]">
+              <Image className="scale-in-center" src={img5} alt=""></Image>
+            </div>
           </div>
         </div>
       </div>
